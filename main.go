37a37f9314995c9b87049090b1ec9d6bcc1e8159
Sanskar531 <sanskargauchanb4@gmail.com>
2023-08-14 19:51:20 +1000

    Support getting servers as cli args

--- main.go
+++ main.go
@@ -3,37 +3,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
+	"strings"
+
 	"github.com/sanskar531/goloadbalance/structs"
 	"github.com/sanskar531/goloadbalance/structs/balancing_algorithms"
-	"net/url"
 )
 
 
-func parseCommandLineArgs() {
-	help := flag.Bool("help", false, "Show help")
-	flag.Parse()
+func parseCommandLineArgs() []*url.URL {
+	servers := flag.String("servers", "", "Show servers");
+	flag.Parse();
 
-	if *help {
-		fmt.Print(`
-			GOPrintlnPrintlnPrintln
-		`)
-		return
+	if *servers == "" {
+		log.Fatal("Please enter at least one server.")
 	}
+
+	serverUrls := strings.Split(*servers, ",")
+	var parsedUrls []*url.URL;
+
+	for _, serverUrl := range serverUrls {
+		parsedUrl, err := url.Parse(serverUrl);
+		if err != nil {
+			log.Fatal("The following url is invalid: ", parsedUrl)
+		}
+
+		parsedUrls = append(parsedUrls, parsedUrl)
+	}
+
+	return parsedUrls
 }
 
 func main() {
-	parseCommandLineArgs()
+	parsedUrls := parseCommandLineArgs();
 
-	serverUrl, err := url.Parse("http://localhost:8000")
-	if err != nil {
-		return
+	var servers []structs.Server;
+
+	for _, parsedUrl := range parsedUrls {
+		servers = append(servers, structs.InitServer(
+			parsedUrl,
+		))
 	}
-
-	servers := []structs.Server{
-			structs.InitServer(
-				serverUrl,
-			),
-	};
 
 	balancer := balancingalgorithms.InitRoundRobin(len(servers))
 
